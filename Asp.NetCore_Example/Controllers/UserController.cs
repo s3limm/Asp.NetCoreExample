023c28bc3cf6919efde1477c7a38105b8fe6ef6d
s3limm <108307150+s3limm@users.noreply.github.com>
2022-10-26 16:37:22 +0300

    learned viewbag , using list

---
+++ Asp.NetCore_Example/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Asp.NetCore_Example.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Asp.NetCore_Example.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult UseViewBag()
+        {
+
+            ViewBag.User = "Yavuz"; //Method 1 
+
+            User user = new User(); //Method 2
+            user.FirstName = "Yavuz";
+            user.LastName = "Emrem";
+            ViewBag.UserName = user;
+
+
+
+            List<User> users = new List<User>() //Method 3
+            {
+                new User() {FirstName = "Yavuz",LastName = "Emrem"},
+                new User() {FirstName = "İnci",LastName = "Emrem"},
+                new User() {FirstName = "Kayra",LastName = "Şahin"},
+                new User() {FirstName = "Jale",LastName = "Emrem"},
+            };
+            ViewBag.Users = users;
+
+            return View();
+        }
+    }
+}
